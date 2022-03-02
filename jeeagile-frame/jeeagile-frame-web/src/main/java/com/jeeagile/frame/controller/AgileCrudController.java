97612d6907e5f5fe0c10ddca08b6bb757c2c0c18
jeeagile <jeeagile@163.com>
2022-03-02 16:43:24 +0800

    新增AgileCrudController基类，并对常用接口进行实现

---
+++ jeeagile-frame/jeeagile-frame-web/src/main/java/com/jeeagile/frame/controller/AgileCrudController.java
@@ -0,0 +1,146 @@
+package com.jeeagile.frame.controller;
+
+import com.jeeagile.core.protocol.annotation.AgileReference;
+import com.jeeagile.core.result.AgileResult;
+import com.jeeagile.core.result.AgileResultCode;
+import com.jeeagile.frame.annotation.AgileDemo;
+import com.jeeagile.frame.annotation.AgileLogger;
+import com.jeeagile.frame.entity.AgileModel;
+import com.jeeagile.frame.entity.AgileValidateModel;
+import com.jeeagile.frame.enums.AgileLoggerType;
+import com.jeeagile.frame.page.AgilePage;
+import com.jeeagile.frame.page.AgilePageable;
+import com.jeeagile.frame.service.IAgileService;
+import com.jeeagile.frame.support.resolver.annotation.SingleRequestBody;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
+
+public abstract class AgileCrudController<I extends IAgileService<T>, T extends AgileModel> extends AgileBaseController {
+    @AgileReference
+    private I agileBaseService;
+
+    @PostMapping(value = "/page")
+    @ApiOperation(value = "分页查询", notes = "分页查询")
+    @AgileLogger(title = "分页查询", type = AgileLoggerType.SELECT)
+    public AgileResult<AgilePage<T>> selectPage(@RequestBody AgilePageable<T> agilePageable) {
+        return this.rtnSuccess(agileBaseService.selectPage(agilePageable));
+    }
+
+    @PostMapping(value = "/list")
+    @ApiOperation(value = "查询列表", notes = "查询列表")
+    @AgileLogger(title = "查询列表", type = AgileLoggerType.SELECT)
+    public AgileResult<List<T>> selectList(@RequestBody T agileModel) {
+        return this.rtnSuccess(agileBaseService.selectList(agileModel));
+    }
+
+    @PostMapping("/detail")
+    @ApiOperation(value = "查看明细", notes = "查看明细")
+    @AgileLogger(title = "查看明细", type = AgileLoggerType.DETAIL)
+    public AgileResult<T> detail(@SingleRequestBody String id) {
+        return this.rtnSuccess(agileBaseService.getById(id));
+    }
+
+    @AgileDemo
+    @PostMapping("/add")
+    @ApiOperation(value = "保存数据", notes = "保存数据")
+    @AgileLogger(title = "保存数据", type = AgileLoggerType.ADD)
+    public AgileResult<T> save(@RequestBody T entity) {
+        this.saveBefore(entity);
+        entity = agileBaseService.saveEntity(entity);
+        this.saveBefore(entity);
+        return this.rtnSuccess(entity);
+    }
+
+    @AgileDemo
+    @PostMapping("/update")
+    @ApiOperation(value = "更新数据", notes = "更新数据")
+    @AgileLogger(title = "更新数据", type = AgileLoggerType.UPDATE)
+    public AgileResult update(@RequestBody T entity) {
+        this.updateBefore(entity);
+        if (agileBaseService.updateEntity(entity)) {
+            this.updateAfter(entity);
+            return this.rtnSuccess("数据更新成功！");
+        } else {
+            return this.rtnError(AgileResultCode.FAIL_OPS_UPDATE, "数据更新失败！");
+        }
+    }
+
+    @AgileDemo
+    @PostMapping("/delete")
+    @ApiOperation(value = "删除数据", notes = "删除数据")
+    @AgileLogger(title = "删除数据", type = AgileLoggerType.DELETE)
+    public AgileResult delete(@SingleRequestBody String id) {
+        this.deleteBefore(id);
+        if (agileBaseService.deleteById(id)) {
+            this.deleteAfter(id);
+            return this.rtnSuccess("数据删除成功！");
+        } else {
+            return this.rtnError(AgileResultCode.FAIL_OPS_DELETE, "数据删除失败！");
+        }
+    }
+
+    /**
+     * 验证实体数据
+     *
+     * @param entity
+     */
+    protected void validateModel(T entity) {
+        if (entity instanceof AgileValidateModel) {
+            ((AgileValidateModel) entity).validate();
+        }
+    }
+
+    /**
+     * 保存前操作
+     *
+     * @param entity
+     */
+    protected void saveBefore(T entity) {
+        this.validateModel(entity);
+    }
+
+    /**
+     * 保存后操作
+     */
+    protected void saveAfter(T entity) {
+        // default method ignored
+    }
+
+    /**
+     * 更新前操作
+     *
+     * @param entity
+     */
+    protected void updateBefore(T entity) {
+        this.validateModel(entity);
+    }
+
+    /**
+     * 更新前操作
+     *
+     * @param entity
+     */
+    protected void updateAfter(T entity) {
+        // default method ignored
+    }
+
+    /**
+     * 删除前操作
+     *
+     * @param id 主键
+     */
+    protected void deleteBefore(String id) {
+        // default method ignored
+    }
+
+    /**
+     * 删除后操作
+     *
+     * @param id
+     */
+    protected void deleteAfter(String id) {
+        // default method ignored
+    }
+}
