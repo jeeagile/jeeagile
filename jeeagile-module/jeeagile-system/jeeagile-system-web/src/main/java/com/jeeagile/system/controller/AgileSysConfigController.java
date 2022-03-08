01579ad7eb9d219f076b25029d9b1a1303db51c7
jeeagile <jeeagile@163.com>
2022-03-08 16:36:46 +0800

    删除多余import

--- jeeagile-module/jeeagile-system/jeeagile-system-web/src/main/java/com/jeeagile/system/controller/AgileSysConfigController.java
+++ jeeagile-module/jeeagile-system/jeeagile-system-web/src/main/java/com/jeeagile/system/controller/AgileSysConfigController.java
@@ -1,26 +1,15 @@
 package com.jeeagile.system.controller;
 
-import com.jeeagile.core.protocol.annotation.AgileReference;
 import com.jeeagile.core.result.AgileResult;
-import com.jeeagile.core.security.annotation.AgileRequiresPermissions;
-import com.jeeagile.frame.annotation.AgileDemo;
-import com.jeeagile.frame.annotation.AgileLogger;
-import com.jeeagile.frame.controller.AgileBaseController;
 import com.jeeagile.frame.controller.AgileCrudController;
-import com.jeeagile.frame.enums.AgileLoggerType;
-import com.jeeagile.frame.page.AgilePage;
-import com.jeeagile.frame.page.AgilePageable;
 import com.jeeagile.frame.support.resolver.annotation.SingleRequestBody;
 import com.jeeagile.system.entity.AgileSysConfig;
 import com.jeeagile.system.service.IAgileSysConfigService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author JeeAgile
