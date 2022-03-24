eea1ab0d7f136d76788b1a851b0b0ba65ae890ba
jeeagile <jeeagile@163.com>
2022-03-24 16:44:51 +0800

    新增租户管理模块

---
+++ jeeagile-ui/src/api/system/tenant.js
@@ -0,0 +1,28 @@
+import { postApi } from '@/utils/axios'
+
+/**
+ * 查询租户列表
+ */
+export const selectTenantPage = data => postApi('/system/tenant/page', data)
+
+/**
+ * 查看租户信息
+ */
+export const detailTenant = tenantId => postApi('/system/tenant/detail', tenantId)
+
+/**
+ * 新增租户信息
+ */
+export const addTenant = data => postApi('/system/tenant/add', data)
+
+/**
+ * 修改租户信息
+ */
+export const updateTenant = data => postApi('/system/tenant/update', data)
+
+/**
+ * 删除租户信息
+ */
+export const deleteTenant = data => postApi('/system/tenant/delete', data)
+
+
