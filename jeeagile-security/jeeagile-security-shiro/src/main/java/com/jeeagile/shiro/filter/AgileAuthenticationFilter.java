322e166f02d7abca21c7a672a3857dd7b57f905b
jeeagile <jeeagile@163.com>
2022-03-24 18:57:04 +0800

    修改Shiro用户权限过滤器使其支持自定义游客注解

--- jeeagile-security/jeeagile-security-shiro/src/main/java/com/jeeagile/shiro/filter/AgileAuthenticationFilter.java
+++ jeeagile-security/jeeagile-security-shiro/src/main/java/com/jeeagile/shiro/filter/AgileAuthenticationFilter.java
@@ -3,7 +3,13 @@ package com.jeeagile.shiro.filter;
 import com.alibaba.fastjson.JSON;
 import com.jeeagile.core.result.AgileResult;
 import com.jeeagile.core.result.AgileResultCode;
+import com.jeeagile.core.security.annotation.AgileRequiresGuest;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -17,6 +23,30 @@ import java.io.PrintWriter;
  */
 @SuppressWarnings("all")
 public class AgileAuthenticationFilter extends FormAuthenticationFilter {
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object object) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        WebApplicationContext webApplicationContext = RequestContextUtils.findWebApplicationContext(httpServletRequest);
+        RequestMappingHandlerMapping requestMappingHandlerMapping = webApplicationContext.getBean(RequestMappingHandlerMapping.class);
+        try {
+            HandlerMethod handlerMethod = (HandlerMethod) requestMappingHandlerMapping.getHandler(httpServletRequest).getHandler();
+
+            AgileRequiresGuest agileRequiresGuest = handlerMethod.getBeanType().getAnnotation(AgileRequiresGuest.class);
+            if (agileRequiresGuest != null) {
+                return true;
+            }
+
+            agileRequiresGuest = handlerMethod.getMethodAnnotation(AgileRequiresGuest.class);
+            if (agileRequiresGuest != null) {
+                return true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return super.isAccessAllowed(request, response, object);
+    }
+
     @Override
     protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
         String requestURI = ((HttpServletRequest) request).getRequestURI();
