c068bf8f0164447b1fcbe535c61b6df619cef350
jeeagile <jeeagile@163.com>
2022-03-28 13:27:42 +0800

    新增获取所有叶子节点方法

--- jeeagile-frame/jeeagile-frame-service/src/main/java/com/jeeagile/frame/service/AgileBaseTreeServiceImpl.java
+++ jeeagile-frame/jeeagile-frame-service/src/main/java/com/jeeagile/frame/service/AgileBaseTreeServiceImpl.java
@@ -1,10 +1,12 @@
 package com.jeeagile.frame.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.jeeagile.core.util.AgileCollectionUtil;
 import com.jeeagile.frame.entity.AgileBaseTreeModel;
 import com.jeeagile.frame.mapper.AgileBaseTreeMapper;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,6 +24,19 @@ public class AgileBaseTreeServiceImpl<M extends AgileBaseTreeMapper<T>, T extend
         }
         wrapper.eq("parent_id", parentId);
         return this.list(wrapper);
+    }
+
+    @Override
+    public List<T> selectAllChildList(Serializable parentUuid) {
+        List<T> allChildList = new ArrayList<>();
+        List<T> childList  = selectChildList(parentUuid);
+        if (AgileCollectionUtil.isNotEmpty(childList)) {
+            allChildList.addAll(childList);
+            childList.forEach(t -> {
+                allChildList.addAll(selectAllChildList(t.getId()));
+            });
+        }
+        return allChildList;
     }
 
     @Override
