5ba9757c89e20e42f0fbe40f84ecdef8d8db2b68
jeeagile <jeeagile@163.com>
2022-03-30 19:25:34 +0800

    提示内容新增访问地址

--- jeeagile-core/src/main/java/com/jeeagile/core/runner/AgileCommandRunner.java
+++ jeeagile-core/src/main/java/com/jeeagile/core/runner/AgileCommandRunner.java
@@ -1,19 +1,38 @@
 package com.jeeagile.core.runner;
 
+import com.jeeagile.core.util.AgileStringUtil;
 import com.jeeagile.core.util.AgileUtil;
+import com.jeeagile.core.util.system.util.AgileSystemUtil;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @Component
 @Order(Integer.MIN_VALUE)
 public class AgileCommandRunner implements CommandLineRunner {
 
+    @Resource
+    private Environment environment;
+
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("=============================================================================");
-        System.out.println("               φ(゜▽゜*)♪    " + AgileUtil.getProjectName() + "启动成功!     ლ(´ڡ`ლ)");
-        System.out.println("                       欢迎使用  JeeAgile  V 2.0.0");
-        System.out.println("=============================================================================");
+        String serverPort = environment.getProperty("server.port");
+        String contextPath = environment.getProperty("server.servlet.context-path");
+        if (AgileStringUtil.isEmpty(serverPort)) {
+            serverPort = "80";
+        }
+        if (AgileStringUtil.isEmpty(contextPath)) {
+            contextPath = "";
+        }
+        String host = AgileSystemUtil.getHostInfo().getAddress();
+        System.out.println("*****************************************************************************");
+        System.out.println("           φ(゜▽゜*)♪    " + AgileUtil.getProjectName() + "启动成功!     ლ(´ڡ`ლ)");
+        System.out.println("     访问地址：");
+        System.out.println("           - Local:    http://localhost:" + serverPort + "/" + contextPath);
+        System.out.println("           - Network:  http://" + host + ":" + serverPort + "/" + contextPath);
+        System.out.println("*****************************************************************************");
     }
 }
