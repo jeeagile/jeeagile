adfe2403bfc939be3906b6cf653d3d93112482ee
jeeagile <jeeagile@163.com>
2022-06-02 15:56:17 +0800

    暂屏蔽knife4j和Swagger相关配置

--- jeeagile-frame/jeeagile-frame-web/src/main/java/com/jeeagile/frame/autoconfigure/AgileSwaggerAutoConfig.java
+++ jeeagile-frame/jeeagile-frame-web/src/main/java/com/jeeagile/frame/autoconfigure/AgileSwaggerAutoConfig.java
@@ -3,14 +3,14 @@ package com.jeeagile.frame.autoconfigure;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.oas.annotations.EnableOpenApi;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
 
 
 /**
@@ -19,26 +19,26 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @description
  */
 @Configuration
-@EnableOpenApi
+//@EnableOpenApi
 @AutoConfigureAfter(AgileWebAutoConfigure.class)
 public class AgileSwaggerAutoConfig {
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.OAS_30)
-                .groupName("JeeAgile")
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.jeeagile"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("JeeAgile")
-                .description("JeeAgile API")
-                .contact(new Contact("JeeAgile", "www.JeeAgile.com", "JeeAgile@qq.com"))
-                .version("2.0.0")
-                .build();
-    }
+//    @Bean
+//    public Docket createRestApi() {
+//        return new Docket(DocumentationType.OAS_30)
+//                .groupName("JeeAgile")
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.jeeagile"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("JeeAgile")
+//                .description("JeeAgile API")
+//                .contact(new Contact("JeeAgile", "www.JeeAgile.com", "JeeAgile@qq.com"))
+//                .version("2.0.0")
+//                .build();
+//    }
 }
